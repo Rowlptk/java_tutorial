8c61be3ef7bb860b5e408b2313e19051c0457848
Rahul Pathak <rahulzha93@gmail.com>
2020-07-08 23:11:26 +0545

    Expressions-Statements

---
+++ 02-Expression-Statements/IfKeyWordAndCodeBlocks.java
@@ -0,0 +1,54 @@
+public class IfKeyWordAndCodeBlocks {
+    public static void main(String[] args) {
+        boolean gameOver = true;
+        int score = 4000;
+        int levelCompleted = 5;
+        int bonus = 100;
+
+        // if(score == 5000){
+        //     System.out.println("Your score was 5000");
+        //     System.out.println("This was executed");
+        // }
+
+        // if(score < 5000 && score > 1000){
+        //     System.out.println("Your Score was less than 5000 but greater than 1000");
+        // } else if(score < 1000) {
+        //     System.out.println("Your score was less than 1000");
+        // } else {
+        //     System.out.println("Got here");
+        // }
+
+        if(gameOver) {
+            int finalScore = score + (levelCompleted + bonus);
+            finalScore += 1000;
+            System.out.println("Your final score was " + finalScore);
+        }
+
+        // int savedFinalScore = finalScore;
+
+        // Print out a second score on the screen with the following
+        // score set to 10000
+        // levelCompleted set to 8
+        // bonus set to 200
+        // But make sure the first printout above still displays as well
+
+        //  boolean newGameOver = true;
+        //  int newScore = 10000;
+        //  int newLevelCompleted = 8;
+        //  int newBonus = 200;
+
+        //  if(newGameOver) {
+        //      int finalScore = newScore + (newLevelCompleted + newBonus);
+        //      System.out.println("Your first score was " + finalScore);
+        //  }
+
+        score = 10000;
+        levelCompleted = 8;
+        bonus = 200;
+
+        if(gameOver) {
+            int finalScore = score + (levelCompleted + bonus);
+            System.out.println("Your final score was " + finalScore);
+        }
+     }
+}
---
+++ 02-Expression-Statements/KeywordsAndExpressions.java
@@ -0,0 +1,21 @@
+public class KeywordsAndExpressions {
+    public static void main(String[] args) {
+        // int int2 = 5;
+        // false, true, null
+
+        // a mile is equal to 1.609344 kilometers
+        double kilometers = (100 * 1.609344);
+        int highScore = 50;
+        if(highScore == 50) {
+            System.out.println("This is an expression");
+        }
+
+
+        // In the following code that I will type below, write down what parts of the code are expressions
+        int score = 100;
+        if (score > 90) {
+            System.out.println("Yes got the high score!");
+            score = 0;
+        }
+    }    
+}
