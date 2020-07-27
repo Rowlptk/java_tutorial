4693a81a4be9730c1824526b82d4ffb8b909d3a0
Rahul Pathak <rahulzha93@gmail.com>
2020-07-27 20:19:42 +0545

    inner and abstract classes & interfaces

---
+++ 07-Inner-And-Abstract-Classes-&-Interfaces/Abstraction/src/com/rahul/Abstraction.java
@@ -0,0 +1,21 @@
+package com.rahul;
+
+public class Abstraction {
+    public static void main(String[] args) {
+        Dog dog = new Dog("Yorkie");
+        dog.breathe();
+        dog.eat();
+        
+//        Bird bird = new Bird("Parrot");
+//        bird.breathe();
+//        bird.eat();
+
+        Parrot parrot = new Parrot("Australian ringneck");
+        parrot.breathe();
+        parrot.eat();
+        parrot.fly();
+        
+        Penguin penguin = new Penguin("Emperor");
+        penguin.fly();
+    }
+}
---
+++ 07-Inner-And-Abstract-Classes-&-Interfaces/Interfaces/src/com/rahul/GearBox.java
@@ -0,0 +1,14 @@
+package com.rahul;
+
+public class GearBox {
+    
+    private boolean clutchIsIn;
+    
+//    public void operateClutch(String inOrOut) {
+//        this.clutchIsIn = inOrOut.equalsIgnoreCase("in");
+//    }
+    
+     public void operateClutch(boolean inOrOut) {
+        this.clutchIsIn = inOrOut;
+    }
+}
