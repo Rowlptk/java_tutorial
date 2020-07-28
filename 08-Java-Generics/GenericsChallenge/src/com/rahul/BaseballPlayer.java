5d2b72675ba9fd1160dc04a0c60060690c70e53b
Rahul Pathak <rahulzha93@gmail.com>
2020-07-29 00:18:11 +0545

    java generics

---
+++ 08-Java-Generics/GenericsChallenge/src/com/rahul/BaseballPlayer.java
@@ -0,0 +1,9 @@
+package com.rahul;
+
+public class BaseballPlayer extends Player {
+
+    public BaseballPlayer(String name) {
+        super(name);
+    }
+    
+}
---
+++ 08-Java-Generics/GenericsChallenge/src/com/rahul/FootballPlayer.java
@@ -0,0 +1,9 @@
+package com.rahul;
+
+public class FootballPlayer extends Player {
+
+    public FootballPlayer(String name) {
+        super(name);
+    }
+    
+}
